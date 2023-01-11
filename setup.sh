c8e439c1f495d532ee1b610ceaca8f1140b84277
mtimani <34349792+mtimani@users.noreply.github.com>
2023-01-11 14:59:16 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+sudo apt-get update && sudo apt-get install subfinder jq nuclei gccgo-go -y
+pip3 install aiodnsbrute
+pip3 install cidrize
+pip2 install hsecscan
+sudo mv recon.py /usr/bin/
+sudo chmod +x /usr/bin/recon.py
+sudo chown $(echo "$USER"):$(echo "$USER") /usr/bin/recon.py 
+sudo mv asset_discovery.py /usr/bin/
+sudo chmod +x /usr/bin/asset_discovery.py
+sudo chown $(echo "$USER"):$(echo "$USER") /usr/bin/asset_discovery.py
+sudo su
+go env -w GO111MODULE=off
+cd /opt/
+git clone https://github.com/hvs-consulting/SANextract
+sudo git clone "https://github.com/jtesta/ssh-audit.git"
+sudo chown -R $(echo "$USER"):$(echo "$USER") /opt/ssh-audit
+cd SANextract
+go build
+chown -R $(echo "$USER"):$(echo "$USER") /opt/SANextract
+wget -c "https://github.com/sensepost/gowitness/releases/download/2.4.2/gowitness-2.4.2-linux-amd64" && mv gowitness* gowitness && chmod +x gowitness && sudo mv gowitness /usr/bin
+wget -c "https://github.com/rverton/webanalyze/releases/download/v0.3.8/webanalyze_0.3.8_Linux_x86_64.tar.gz"
+tar -xzvf webanalyze_0.3.8_Linux_x86_64.tar.gz
+./webanalyze -update
+rm -rf webanalyze_0.3.8_Linux_x86_64.tar.gz technologies.json
+sudo mv webanalyze /usr/bin
+wget -c "https://github.com/lc/gau/releases/download/v2.1.2/gau_2.1.2_linux_amd64.tar.gz"
+tar -xzvf gau_2.1.2_linux_amd64.tar.gz
+sudo mv gau /usr/bin/
+d /opt
+sudo git clone https://github.com/ShutdownRepo/httpmethods
+sudo chown -R $(echo "$USER"):$(echo "$USER") /opt/httpmethods
+cd /opt/httpmethods
+sudo python3 setup.py install
+rm -rf gau_2.1.2_linux_amd64.tar.gz LICENSE README.md
