0793ea73ab861cdaf229d7e8b55c340c3d45fa3a
mtimani <34349792+mtimani@users.noreply.github.com>
2023-12-11 17:16:08 +0100

    Setup tested on Exegol and Kali

--- setup.sh
+++ setup.sh
@@ -1,49 +1,319 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 
-sudo apt-get update && sudo apt-get install subfinder jq nuclei gccgo-go whois eyewitness -y
+# Colors setup
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+initial_dir=$(pwd)
 
+#Check if script is ran as root
+if [ "$EUID" -ne 0 ]
+  then echo "${RED}Please run as root!${NC}"
+  exit
+fi
+
+# Check OS
+OS=$(lsb_release -a 2>/dev/null | grep 'Distributor ID' | awk '{print $3}')
+if [ "$(echo $HOSTNAME | awk -F '-' '{print $1}')" = "exegol" ]; then
+    OS="Exegol"
+    source /opt/.exegol_aliases
+fi
+
+# Echo information
+if [ "$OS" = "Kali" ]; then
+    echo "\n${GREEN}Kali Linux detected. The script can proceed with installation${NC}\n"
+elif [ "$OS" = "Exegol" ] || [ "$OS" = "Ubuntu" ] || [ "$OS" = "Debian" ]; then
+    echo "\n${GREEN}$OS detected. The script can proceed with installation${NC}\n"
+else
+    echo "\n${RED}This script has to be ran in Kali Linux, Exegol, Debian or Ubuntu! Other systems are not yet supported${NC}\n"
+    exit 1
+fi
+
+# Update repositories
+apt-get update
+
+# Install required packages via apt
+
+## Install jq
+command -v "jq" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        apt-get install jq -y
+    fi
+
+## Install git
+command -v "git" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        apt-get install git -y
+    fi
+
+## Install go
+command -v "go" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ] || [ "$OS" = "Debian" ] || [ "$OS" = "Exegol" ]; then
+            apt-get install gccgo-go -y
+            if [ "$OS" = "Kali" ]; then
+                export PATH=$PATH:/root/go/bin
+            fi
+        elif [ "$OS" = "Ubuntu" ]; then
+            apt-get install golang-go  -y
+        fi
+    fi
+
+## Install whois
 command -v "whois" >/dev/null 2>&1
     if [[ $? -ne 0 ]]; then
-        sudo apt-get install whois
-        exit 1
+        apt-get install whois -y
     fi
-pip3 install aiodnsbrute cidrize alive-progress wafw00f tldextract
+
+## Install curl
+command -v "curl" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        apt-get install curl -y
+    fi
+
+## Install wget
+command -v "wget" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        apt-get install wget -y
+    fi
+
+## Install pip2
+command -v "pip2" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        wget https://gist.githubusercontent.com/anir0y/a20246e26dcb2ebf1b44a0e1d989f5d1/raw/a9908e5dd147f0b6eb71ec51f9845fafe7fb8a7f/pip2%2520install -O run.sh 
+        chmod +x run.sh
+        ./run.sh
+        rm -rf run.sh
+    fi
+
+## Install nuclei
+command -v "nuclei" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ]; then
+            apt-get install nuclei -y
+        else
+            go env -w GO111MODULE=off
+            go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+            nuclei -update
+            nuclei -ut
+        fi
+    fi
+
+## Install eyewitness
+command -v "eyewitness" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ]; then
+            apt-get install eyewitness -y
+        elif [ "$OS" = "Debian" ]; then
+            git clone https://github.com/RedSiege/EyeWitness.git
+            current_dir=$(pwd)
+            cd EyeWitness/Python/setup
+            chmod +x setup.sh
+            ./setup.sh
+            cd $current_dir
+            rm -rf EyeWitness
+        fi
+    fi
+
+## Install subfinder
+command -v "subfinder" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ]; then
+            apt-get install subfinder -y
+        else
+            go env -w GO111MODULE=off
+            go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+        fi
+    fi
+
+## Install SANextract
+command -v "SANextract" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        git clone https://github.com/hvs-consulting/SANextract
+        current_dir=$(pwd)
+        cd SANextract
+        if [ "$OS" = "Kali" ]; then
+            export GO111MODULE="on"
+            go mod init SANextract
+            go build
+        else
+            go env -w GO111MODULE=off
+            go mod init SANextract
+            go build
+        fi
+        chown $(echo "$USER"):$(echo "$USER") SANextract
+        mv SANextract /usr/bin/
+        cd $current_dir
+        rm -rf SANextract
+    fi
+
+## Install gowitness
+command -v "gowitness" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        wget -c "https://github.com/sensepost/gowitness/releases/download/2.4.2/gowitness-2.4.2-linux-amd64"
+        mv gowitness* gowitness
+        chmod +x gowitness
+        mv gowitness /usr/bin
+    fi
+
+## Install webanalyze
+command -v "webanalyze" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ]; then
+            export GO111MODULE="on"
+            go install -v github.com/rverton/webanalyze/cmd/webanalyze@latest
+            mv /root/go/bin/webanalyze /usr/bin/
+        else
+            go env -w GO111MODULE=off
+            go install -v github.com/rverton/webanalyze/cmd/webanalyze@latest
+        fi
+        webanalyze -update
+    fi
+
+## Install gau
+command -v "gau" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ]; then
+            apt-get install getallurls -y
+        else
+            go env -w GO111MODULE=off
+            go install github.com/lc/gau/v2/cmd/gau@latest
+        fi
+    fi
+
+## Install httpmethods
+command -v "httpmethods" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        git clone https://github.com/ShutdownRepo/httpmethods
+        current_dir=$(pwd)
+        chown -R $(echo "$USER"):$(echo "$USER") httpmethods
+        cd httpmethods
+        if [ "$OS" = "Kali" ]; then
+            rm -rf assets/ wordlists/
+        fi
+        python3 setup.py install
+        cd $current_dir
+        rm -rf httpmethods
+    fi
+
+## Install httpx
+command -v "httpx" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        if [ "$OS" = "Kali" ]; then
+            export GO111MODULE="on"
+            go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+        else
+            go env -w GO111MODULE=off
+            go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+        fi
+    fi
+
+## Install httpx
+command -v "findomain" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
+        unzip findomain-linux.zip
+        chmod +x findomain
+        mv findomain /usr/bin/findomain
+        rm -rf findomain-linux.zip
+    fi
+
+# Install required packages via pip2 and pip3
+pip3 install aiodnsbrute cidrize alive-progress wafw00f tldextract termcolor --break-system-packages
 pip2 install hsecscan
-sudo mv recon.py /usr/bin/
-sudo chmod +x /usr/bin/recon.py
-sudo chown $(echo "$USER"):$(echo "$USER") /usr/bin/recon.py 
-sudo mv asset_discovery.py /usr/bin/
-sudo chmod +x /usr/bin/asset_discovery.py
-sudo chown $(echo "$USER"):$(echo "$USER") /usr/bin/asset_discovery.py
-sudo su
-go env -w GO111MODULE=off
-cd /opt/
-git clone https://github.com/hvs-consulting/SANextract
-sudo git clone "https://github.com/jtesta/ssh-audit.git"
-sudo chown -R $(echo "$USER"):$(echo "$USER") /opt/ssh-audit
-cd SANextract
-chown -R $(echo "$USER"):$(echo "$USER") /opt/SANextract
-go mod init SANextract
-go build
-cd /opt/
-wget -c "https://github.com/sensepost/gowitness/releases/download/2.4.2/gowitness-2.4.2-linux-amd64" && mv gowitness* gowitness && chmod +x gowitness && sudo mv gowitness /usr/bin
-wget -c "https://github.com/rverton/webanalyze/releases/download/v0.3.8/webanalyze_0.3.8_Linux_x86_64.tar.gz"
-go install -v github.com/rverton/webanalyze/cmd/webanalyze@lates
-webanalyze -update
-wget -c "https://github.com/lc/gau/releases/download/v2.1.2/gau_2.1.2_linux_amd64.tar.gz"
-tar -xzvf gau_2.1.2_linux_amd64.tar.gz
-rm -rf gau_2.1.2_linux_amd64.tar.gz
-sudo mv gau /usr/bin/
-cd /opt
-sudo git clone https://github.com/danielmiessler/SecLists.git
-sudo git clone https://github.com/ShutdownRepo/httpmethods
-sudo chown -R $(echo "$USER"):$(echo "$USER") /opt/httpmethods
-cd /opt/httpmethods
-sudo python3 setup.py install
-rm -rf LICENSE README.md
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
-unzip findomain-linux.zip
-chmod +x findomain
-sudo mv findomain /usr/bin/findomain
-rm -rf findomain-linux.zip
+
+# Download ssh-audit
+if [ ! -d ' /opt/ssh-audit' ]; then
+    cd /opt/
+    git clone "https://github.com/jtesta/ssh-audit.git"
+    chown -R $(echo "$USER"):$(echo "$USER") /opt/ssh-audit
+fi
+
+# Download SecLists
+if [ ! -d ' /opt/SecLists' ]; then
+    cd /opt/
+    git clone https://github.com/danielmiessler/SecLists.git
+    chown -R $(echo "$USER"):$(echo "$USER") /opt/SecLists
+fi
+
+# Download testssl
+if [ ! -d ' /opt/testssl.sh' ]; then
+    cd /opt/
+    git clone https://github.com/drwetter/testssl.sh.git
+    chown -R $(echo "$USER"):$(echo "$USER") /opt/testssl.sh
+fi
+
+# Replace global variables in recon.py
+## Variable init
+cd $initial_dir
+httpmethods_location=$(which httpmethods)
+webanalyze_location=$(which webanalyze)
+if [ "$OS" = "Kali" ]; then
+    gau_location=$(which getallurls)
+else
+    gau_location=$(which gau)
+fi
+
+## Actual replacement
+old_location="/opt/httpmethods/httpmethods.py"
+if [[ $httpmethods_location == *"aliased to"* ]]; then
+    httpmethods_location=$(which httpmethods | awk '{print $5}')
+fi
+sed -i -e "s@$old_location@$httpmethods_location@" recon.py
+old_location="/usr/bin/webanalyze"
+sed -i -e "s@$old_location@$webanalyze_location@" recon.py
+old_location="/usr/bin/gau"
+sed -i -e "s@$old_location@$gau_location@" recon.py
+
+# Replace global variables in asset_discovery.py
+## Variable init
+cd $initial_dir
+sanextract_location=$(which SANextract)
+webanalyze_location=$(which webanalyze)
+if [ "$OS" = "Kali" ]; then
+    gau_location=$(which getallurls)
+else
+    gau_location=$(which gau)
+fi
+gowitness_location=$(which gowitness)
+findomain_location=$(which findomain)
+eyewitness_location=$(which eyewitness)
+
+## Actual replacement
+old_location="/opt/SANextract/SANextract"
+sed -i -e "s@$old_location@$sanextract_location@" asset_discovery.py
+old_location="/usr/bin/webanalyze"
+sed -i -e "s@$old_location@$webanalyze_location@" asset_discovery.py
+old_location="/usr/bin/gau"
+sed -i -e "s@$old_location@$gau_location@" asset_discovery.py
+old_location="/usr/bin/gowitness"
+sed -i -e "s@$old_location@$gowitness_location@" asset_discovery.py
+old_location="/usr/bin/findomain"
+sed -i -e "s@$old_location@$findomain_location@" asset_discovery.py
+old_location="/usr/bin/eyewitness"
+if [[ $eyewitness_location == *"aliased to"* ]]; then
+    eyewitness_location=$(which eyewitness | awk '{print $5}')
+    sed -i -e "s@$old_location@$eyewitness_location@" asset_discovery.py
+else
+    sed -i -e "s@$old_location@$eyewitness_location@" asset_discovery.py
+fi
+
+
+# Move scripts to /usr/bin/
+cd $initial_dir
+mv recon.py /usr/bin/
+chmod +x /usr/bin/recon.py
+chown $(echo "$USER"):$(echo "$USER") /usr/bin/recon.py
+mv asset_discovery.py /usr/bin/
+chmod +x /usr/bin/asset_discovery.py
+chown $(echo "$USER"):$(echo "$USER") /usr/bin/asset_discovery.py
+mv root_domains_extractor.py /usr/bin/
+chmod +x /usr/bin/root_domains_extractor.py
+chown $(echo "$USER"):$(echo "$USER") /usr/bin/root_domains_extractor.py
+mv whois_stats.py /usr/bin/
+chmod +x /usr/bin/whois_stats.py
+chown $(echo "$USER"):$(echo "$USER") /usr/bin/whois_stats.py
+
+if [ "$OS" = "Kali" ]; then
+    echo "\n${RED}Warning! Add the following directory to your PATH variable: /root/go/bin${NC}\n"
+fi
